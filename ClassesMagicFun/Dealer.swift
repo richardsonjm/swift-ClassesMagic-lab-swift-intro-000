d2b16b6a72163361fbe04c019b2a840bc0472517
John Richardson <richardsonjm@gmail.com>
2016-11-18 09:49:25 -0500

    WIP

--- ClassesMagicFun/Dealer.swift
+++ ClassesMagicFun/Dealer.swift
@@ -9,4 +9,55 @@
 import Foundation
 
 class Dealer {
+    var deck: Deck
+    var player: Player
+    var house: House
+    var winner: Player?
+    var currentBet: UInt
+    
+    init() {
+        self.deck = Deck()
+        self.player = Player(name: "Player")
+        self.house = House()
+        self.currentBet = 0
+    }
+    
+    
+    func deal() {
+        winner = nil
+        player.hand.append(deck.drawCard()!)
+        house.hand.append(deck.drawCard()!)
+        player.hand.append(deck.drawCard()!)
+        house.hand.append(deck.drawCard()!)
+        if player.isBlackjack {
+            winner = player
+        } else if house.isBlackjack {
+            winner = house
+        }
+    }
+    
+    func turn(_ player: Player) {
+        while (!player.isBlackjack && player.willHit(currentBet)) {
+            player.hand.append(deck.drawCard()!)
+        }
+    }
+    
+    func award() {
+        if (winner?.name == "Player") {
+            player.win(currentBet)
+            house.lose(currentBet)
+        } else if (winner?.name == "House"){
+            house.win(currentBet)
+            player.lose(currentBet)
+        }
+    }
 }
+
+//Create a deck property of type Deck to store the deck used to play the game.
+//Create a player property of type Player to store the person who is playing the game.
+//Create a house property of type House to store the house participating in the game.
+//Create a winner property of type Player?. It should return the winner of the game if the game is over, or nil if the game is still in progress.
+//Create an initializer that takes no parameters. It should create a new deck, a new player named "Player", and a House.
+//Implement the method deal(). This will be the first deal of the game. The player should be dealt two cards, and the house should get two cards. If either player has blackjack (21), the winner property should be set to the winner of the game.
+//Implement a turn(_:) method. This should take a single parameter, player, which is the Player whose turn should be taken. The player should be dealt cards until they decide to stay (take no more cards).
+//Implement a method award(_:). This method should give the winner the current bet, and deduct the current from the losing player.
--- ClassesMagicFun/House.swift
+++ ClassesMagicFun/House.swift
@@ -9,4 +9,15 @@
 import Foundation
 
 class House: Player {
+    init() {
+        super.init(name: "House")
+    }
+    
+    override func willHit(_ amount: UInt) -> Bool {
+        return handValue < 17
+    }
 }
+
+
+//Create an initializer that takes no parameters. It should call super.init() (the Player superclass's initializer) with the player name of "House".
+//    Override willHit(_:). Like its superclass's implementation, it takes one parameter, a UInt called bet. It should return true if the House's current hand value is below 17.
